7775ce6af5ce41ac9cbac838d02c10027e7a1e45
Alexey Moiseenko <alex.moisk@gmail.com>
2020-06-03 10:52:30 +0300

    My first action is ready

---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const path = require('path');
+
+const packageJson = require(path.resolve(
+    process.env.GITHUB_WORKSPACE,
+    'package.json'
+));
+
+const currentRef = process.env.GITHUB_REF;
+const releaseBranches = packageJson.release.branches;
+const currentBranch = releaseBranches
+                        .find(branch => currentRef.includes(branch.name));
+
+const currentTag = releaseBranches
+                    .filter(branch => branch.channel !== undefined)
+                    .find(branch => currentRef.includes(branch.channel))
+
+let channel = 'latest';
+
+if (!currentBranch && currentTag) {
+    channel = currentTag.channel;
+} else if (currentBranch && currentBranch.name !== 'master') {
+    channel = currentBranch.channel
+}
+
+console.log('channel', channel)
+
+core.exportVariable('RELEASE_CHANNEL', channel);
